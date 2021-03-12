1acd91b816923a859ea52dbb1c3b2e066c156fd6
Vinicius Emmanuel Nitzke <vinicius.nitzke@ambevtech.com.br>
2021-03-12 08:39:45 -0300

    Alteration name

--- Generate_csv/run.py
+++ Generate_csv/run.py
@@ -1,4 +1,4 @@
-from writer import search_champions, generate_csv_champions
+from Generate_csv.generate_serie_a import search_champions, generate_csv_champions
 
 if __name__ == '__main__':
     champion = search_champions()
